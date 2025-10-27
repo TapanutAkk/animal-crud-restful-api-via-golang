be5d0674216c66a2c9daf1815e2656af52e56039
TapanutAkk <tapanut.akk@gmail.com>
2025-10-27 13:51:21 +0700

    build generate jwt api and add it in header for each api

--- main.go
+++ main.go
@@ -3,30 +3,49 @@ package main
 import (
 	"animal-crud-api/controllers"
 	"animal-crud-api/database"
+	"animal-crud-api/middlewares"
+	"animal-crud-api/utils"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
-func main() {
-    if err := godotenv.Load(); err != nil {
-        log.Println("Error loading .env file, using default environment variables for port.")
+func Login(c *gin.Context) {
+    userID := uint(1) 
+    
+    token, err := utils.GenerateToken(userID)
+    if err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to generate token"})
+        return
     }
+
+    c.JSON(http.StatusOK, gin.H{"token": token, "message": "Login successful. Use this token for protected routes."})
+}
+
+func main() {
+	if err := godotenv.Load(); err != nil { 
+		log.Println("Error loading .env file. Using system environment variables or defaults.") 
+	}
 
     database.Connect()
 
 	r := gin.Default()
 
 	api := r.Group("/api")
+	api.POST("/login", Login)
+
+	protected := api.Group("/animals") 
+    protected.Use(middlewares.AuthRequired())
     {
-		api.POST("/animals", controllers.CreateAnimal)
-        api.GET("/animals", controllers.FindAnimals)
-        api.GET("/animals/:id", controllers.FindAnimal)
-        api.PUT("/animals/:id", controllers.UpdateAnimal)
-        api.DELETE("/animals/:id", controllers.DeleteAnimal)
+		protected.POST("", controllers.CreateAnimal)
+        protected.GET("", controllers.FindAnimals)
+        protected.GET("/:id", controllers.FindAnimal)
+        protected.PUT("/:id", controllers.UpdateAnimal)
+        protected.DELETE("/:id", controllers.DeleteAnimal)
 	}
 
     port := os.Getenv("APP_PORT")
